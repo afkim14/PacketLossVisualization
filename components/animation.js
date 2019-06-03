16dfd86575db8d0ddb672b50634f8c0f13ed5961
afkim14 <andreskim2018@u.northwestern.edu>
2019-06-03 10:58:07 -0500

    initial commit

---
+++ components/animation.js
@@ -0,0 +1,153 @@
+const React = require('react');
+import Visualization from './visualization';
+
+const COLORS = ["#EB2f39", "#2C53FF", "#26BD67"];
+function Square(xfixed, xchange, ychange, currColor) {
+  this.xfixed = xfixed;
+  this.xchange = xchange;
+  this.ychange = ychange;
+  this.currColor = currColor;
+}
+
+class Animation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { currNumSquares: this.props.numSquares, currValue: 0, squares: [] };
+    this.updateAnimationState = this.updateAnimationState.bind(this);
+  }
+
+  componentDidMount() {
+    if (this.state.squares.length == 0) {
+      this.setState({squares: this.initializeSquares(0)});
+    }
+    this.rAF = requestAnimationFrame(this.updateAnimationState);
+  }
+
+  initializeSquares(currValue) {
+    var squares = [];
+    var numSquares = this.props.numSquares;
+    if (currValue == 6) {
+      numSquares = numSquares * 2;
+    }
+    this.setState({currNumSquares: numSquares});
+
+    for (var i = 0; i < numSquares; i++) {
+      if (currValue == 5 || currValue == 6 || currValue == 7 || currValue == 8) {
+        squares.push(new Square(false, 0, 0, COLORS[Math.floor(Math.random() * COLORS.length)]));
+      } else {
+        squares.push(new Square(false, 0, 0, "#07ADBD"));
+      }
+    }
+    return squares;
+  }
+
+  updateAnimationState() {
+    var endpoint = 380;
+    var speed = 2;
+    if (this.state.currValue != this.props.value) {
+      this.setState({currValue: this.props.value, squares: this.initializeSquares(this.props.value)});
+    }
+
+    if (this.state.currValue == 4) {
+      if (this.state.currNumSquares != this.props.numSquares) {
+        this.setState({currNumSquares: this.props.numSquares, squares: this.initializeSquares(this.state.currValue)});
+      }
+    }
+
+    for (var i = 0; i < this.state.squares.length; i++) {
+      if (this.state.squares[i].xchange - (i * 50) >= endpoint || this.state.squares[i].ychange - (i * 50) >= endpoint - 150) {
+        var newSquares = this.state.squares;
+        newSquares[i].xchange = 0;
+        newSquares[i].ychange = 0;
+        newSquares[i].xfixed = false;
+        newSquares[i].currColor = "#07ADBD";
+        if (this.props.value == 5 || this.props.value == 6 || this.props.value == 7 || this.props.value == 8) {
+          newSquares[i].currColor = COLORS[Math.floor(Math.random() * COLORS.length)];
+        }
+        this.setState({squares: newSquares});
+      }
+
+      if (this.props.value == 1) {
+        var newSquares = this.state.squares;
+        newSquares[i].xchange += speed;
+        newSquares[i].xfixed = false;
+        this.setState({squares: newSquares});
+      } else if (this.props.value == 2 || this.props.value == 3 || this.props.value == 4 || this.props.value == 6 || this.props.value == 8) {
+        var xfixed = this.state.squares[i].xfixed;
+        if (this.state.squares[i].xchange - (i * 50) == (endpoint/2) && !xfixed) {
+          var packetloss_prob = 100;
+          if (this.props.value == 8) {
+            if (this.state.squares[i].currColor != COLORS[0]) {
+              packetloss_prob = 0;
+            }
+          } else if (this.props.value == 4) {
+            if (this.props.numSquares == 6) {
+              packetloss_prob = 40;
+            } else if (this.props.numSquares > 7) {
+              packetloss_prob = 65;
+            } else {
+              packetloss_prob = 0;
+            }
+          } else if (this.props.value == 6) {
+            packetloss_prob = 40;
+          }
+          var random = Math.floor(Math.random() * 100);
+          if (random <= packetloss_prob) {
+            xfixed = true;
+          }
+        }
+
+        if (this.props.value == 3) {
+          speed = 0.5;
+        }
+
+        var newSquares = this.state.squares;
+        if (!xfixed) {
+          newSquares[i].xchange += speed;
+          newSquares[i].xfixed = false;
+          this.setState({ squares: newSquares });
+        } else {
+          newSquares[i].ychange += speed;
+          newSquares[i].xfixed = true;
+          newSquares[i].currColor = "#EBEBEB";
+          this.setState({ squares: newSquares });
+        }
+      } else if (this.props.value == 5 || this.props.value == 7) {
+        var newSquares = this.state.squares;
+        var xfixed = this.state.squares[i].xfixed;
+        if (this.state.squares[i].xchange - (i * 50) == (endpoint/2) && !xfixed) {
+          if ((this.props.red_filter && newSquares[i].currColor == COLORS[0]) ||
+              (this.props.blue_filter && newSquares[i].currColor == COLORS[1]) ||
+              (this.props.green_filter && newSquares[i].currColor == COLORS[2]))
+          {
+            xfixed = true;
+          }
+        }
+
+        if (!xfixed) {
+          newSquares[i].xchange += speed;
+          newSquares[i].xfixed = false;
+          this.setState({ squares: newSquares });
+        } else {
+          newSquares[i].ychange += speed;
+          newSquares[i].xfixed = true;
+          newSquares[i].currColor = "#EBEBEB";
+          this.setState({ squares: newSquares });
+        }
+      }
+    }
+
+    this.rAF = requestAnimationFrame(this.updateAnimationState);
+  }
+
+  componentWillUnmount() {
+    cancelAnimationFrame(this.rAF);
+  }
+
+  render() {
+    const { hasError, idyll, updateProps, ...props } = this.props;
+    return <Visualization step={this.props.value} squares={this.state.squares}/>;
+  }
+}
+
+module.exports = Animation;
---
+++ components/visualization.js
@@ -0,0 +1,103 @@
+const React = require('react');
+
+class Visualization extends React.Component {
+  constructor(props) {
+    super(props);
+    this.canvasRef = React.createRef();
+    this.canvasWidth = "500";
+    this.canvasHeight = "250";
+  }
+
+  componentDidUpdate() {
+    const { step, squares } = this.props;
+    if (step == 1 || step == 2) {
+      const canvas = this.canvasRef.current;
+      const ctx = canvas.getContext('2d');
+      const width = canvas.width;
+      const height = canvas.height;
+
+      ctx.save();
+      ctx.beginPath();
+      ctx.clearRect(0, 0, width, height);
+      ctx.translate(width / 2, height / 2);
+      ctx.fillStyle = '#07ADBD';
+      ctx.fillRect(Math.floor(-width / 4) - 200, Math.floor(-height / 6) + 30, width, 3);
+      ctx.strokeStyle = squares[0].currColor;
+      ctx.strokeRect(Math.floor(-width / 4) - 160 + squares[0].xchange, Math.floor(-height / 10) + squares[0].ychange, 30, 30);
+      ctx.fillStyle = '#38BA7D';
+      ctx.fillRect(Math.floor(-width / 4), Math.floor(-height / 6), 70, 70);
+      ctx.restore();
+    } else if (step == 3 || step == 4 || step == 5 || step == 7 || step == 8) {
+      const canvas = this.canvasRef.current;
+      const ctx = canvas.getContext('2d');
+      const width = canvas.width;
+      const height = canvas.height;
+
+      ctx.save();
+      ctx.beginPath();
+      ctx.clearRect(0, 0, width, height);
+      ctx.translate(width / 2, height / 2);
+      ctx.fillStyle = '#07ADBD';
+      ctx.fillRect(Math.floor(-width / 4) - 200, Math.floor(-height / 6) + 30, width, 3);
+      for (var i = 0; i < squares.length; i++) {
+        ctx.strokeStyle = squares[i].currColor;
+        ctx.strokeRect(Math.floor(-width / 4) - (160 + (i * 50)) + squares[i].xchange, Math.floor(-height / 10) + squares[i].ychange, 30, 30);
+      }
+      ctx.fillStyle = '#38BA7D';
+      ctx.fillRect(Math.floor(-width / 4), Math.floor(-height / 6), 70, 70);
+      ctx.restore();
+    } else if (step == 6) {
+      const canvas = this.canvasRef.current;
+      const ctx = canvas.getContext('2d');
+      const width = canvas.width;
+      const height = canvas.height;
+
+      ctx.save();
+      ctx.beginPath();
+      ctx.clearRect(0, 0, width, height);
+      ctx.translate(width / 2, height / 2);
+
+      // INTENTIONAL
+      ctx.fillStyle = '#07ADBD';
+      ctx.fillRect(Math.floor(-width / 4) - 200, Math.floor(-height / 6) - 30, width, 3);
+      for (var i = 0; i < squares.length / 2; i++) {
+        ctx.strokeStyle = squares[i].currColor;
+        ctx.strokeRect(Math.floor(-width / 4) - (160 + (i * 50)) + squares[i].xchange, Math.floor(-height / 10) - 60 - squares[i].ychange, 30, 30);
+      }
+      ctx.fillStyle = '#eb2f39';
+      ctx.fillRect(Math.floor(-width / 4), Math.floor(-height / 6) - 60, 70, 70);
+
+      // UNITENTIONAL
+      ctx.fillStyle = '#07ADBD';
+      ctx.fillRect(Math.floor(-width / 4) - 200, Math.floor(-height / 6) + 70, width, 3);
+      for (var i = 0; i < squares.length / 2; i++) {
+        ctx.strokeStyle = squares[i].currColor;
+        ctx.strokeRect(Math.floor(-width / 4) - (160 + (i * 50)) + squares[i].xchange, Math.floor(-height / 10) + 40 + squares[i].ychange, 30, 30);
+      }
+      ctx.fillStyle = '#38BA7D';
+      ctx.fillRect(Math.floor(-width / 4), Math.floor(-height / 6) + 40, 70, 70);
+
+      ctx.restore();
+    }
+  }
+
+  render() {
+    if (this.props.step == 0) {
+      return (
+        <img src={"../static/images/packet-01.png"} />
+      );
+    } else if (this.props.step == 9) {
+      return (
+
+      );
+    } else {
+      return (
+        <div>
+          <canvas width={this.canvasWidth} height={this.canvasHeight} ref={this.canvasRef} />
+        </div>
+      );
+    }
+  }
+}
+
+module.exports = Visualization;
